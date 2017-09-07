30a4cbce7804587cd45216cbaccb3ced7d166d89
Martin Zachrison <martinzachrison@spotify.com>
2017-09-08 00:25:17 +0200

    Added sections and start time.

--- app/actors/TrainActor.scala
+++ app/actors/TrainActor.scala
@@ -1,16 +1,36 @@
 package actors
 
-import actors.TrainActor.Tick
+import actors.TrainActor.{Tick, Time}
 import akka.actor.{Actor, ActorLogging, Props}
 
 object TrainActor {
-  def props = Props[TrainActor]
-  case class Tick(time:Long)
+  type Time = Long
+
+  def props(start: Time, sections: List[Section]) = Props(new TrainActor(start: Time, sections: List[Section]))
+
+  case class Tick(time: Time)
+
 }
 
-class TrainActor extends  Actor with ActorLogging{
+case class Section(time: Time)
+
+class TrainActor(start: Time, sections: List[Section]) extends Actor with ActorLogging {
   override def receive = {
     case Tick(time) ⇒
-      log.info(s" time is $time")
+      log.info(s" time is $time, start:$start")
+      if (time == start) {
+        val section :: tail = sections
+        log.info(s"next section $section")
+        context.become(onSection(section.time, tail))
+      }
+  }
+
+
+  def onSection(left: Time, sections: List[Section]): Receive = {
+    case Tick(time) if left > 0 ⇒
+      log.info(s"Time left $left")
+      context.become(onSection(left - 1, sections))
+    case Tick(time) ⇒
+      log.info(s"Time to change section: $sections")
   }
 }
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,30 +2,35 @@ package controllers
 
 import javax.inject._
 
-import actors.TrainActor
+import actors.{Section, TrainActor}
 import actors.TrainActor.Tick
 import akka.actor.{ActorRef, ActorSystem}
 import play.api._
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class HomeController @Inject()(system: ActorSystem, cc: ControllerComponents) extends AbstractController(cc) {
 
-  private val train: ActorRef = system.actorOf(TrainActor.props)
+  val log: Logger = Logger(this.getClass)
+  private val train: ActorRef = system.actorOf(TrainActor.props(3, List(Section(2), Section(1))))
 
-  train ! Tick(1)
+  for (time ← 0 to 10) {
+    val tick = Tick(time)
+    log.info(s"Sending $tick")
+    train ! tick
+  }
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
