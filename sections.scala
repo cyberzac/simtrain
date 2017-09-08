43a8d6efa55e87e962d49f85531667e85ab69a88
Daniel Sahlberg <daniel.l.sahlberg@gmail.com>
2017-09-08 16:52:56 +0200

    nytt format

--- sections.scala
+++ sections.scala
@@ -1,12 +1,21 @@
-var sectionFln-Hno = system.actorOf(SectionActor.props(Section("Fln-Hno", 1)))
-var sectionHno-Orn = system.actorOf(SectionActor.props(Section("Hno-Orn", 1)))
-var sectionBlg-Orn = system.actorOf(SectionActor.props(Section("Blg-Orn", 1)))
-var sectionBlg-Blg = system.actorOf(SectionActor.props(Section("Blg-Blg", 999)))
-var sectionBlg-Sau = system.actorOf(SectionActor.props(Section("Blg-Sau", 1)))
-var sectionGtf-Sau = system.actorOf(SectionActor.props(Section("Gtf-Sau", 1)))
-var sectionGtf-St = system.actorOf(SectionActor.props(Section("Gtf-St", 1)))
-var sectionSt-St = system.actorOf(SectionActor.props(Section("St-St", 999)))
-var sectionSt-Vhy = system.actorOf(SectionActor.props(Section("St-Vhy", 1)))
-var sectionHdm-Vhy = system.actorOf(SectionActor.props(Section("Hdm-Vhy", 1)))
-var sectionHdm-Hdm = system.actorOf(SectionActor.props(Section("Hdm-Hdm", 999)))
-var sectionHdm-Snb = system.actorOf(SectionActor.props(Section("Hdm-Snb", 1)))
+package controllers
+
+import model.Section
+
+object Sections {
+
+  val sections = List(
+    Section("FlnHno", 1),
+    Section("HnoOrn", 1),
+    Section("BlgOrn", 1),
+    Section("BlgBlg", 999),
+    Section("BlgSau", 1),
+    Section("GtfSau", 1),
+    Section("GtfSt", 1),
+    Section("StSt", 999),
+    Section("StVhy", 1),
+    Section("HdmVhy", 1),
+    Section("HdmHdm", 999),
+    Section("HdmSnb", 1)
+  )
+}
