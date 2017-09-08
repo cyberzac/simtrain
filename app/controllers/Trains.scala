9ad1a8ab3c85428f4a6c77e75a25598d8fbb37e7
Martin Zachrison <martinzachrison@spotify.com>
2017-09-08 18:34:12 +0200

    Not passing tests

---
+++ app/controllers/Trains.scala
@@ -0,0 +1,10 @@
+package controllers
+
+
+object Trains {
+
+  val trains = List(
+    Train(17, 1000, List(TrainSection(100, "Arbn-St"))),
+    Train(20, 1000, List(TrainSection(100, "Arbn-St")))
+  )
+}
--- test/actors/TrainActorSpec.scala
+++ test/actors/TrainActorSpec.scala
@@ -17,11 +17,12 @@ class TrainActorSpec(_system: ActorSystem) extends TestKit(_system) with Implici
 
   val log: Logger = Logger(this.getClass)
   val startTime = 2
-  val sectionActor = TestProbe()
-  val section1 = Section("Orn-Vhy",1)
-  val section2 = Section("Blg-Orn",1)
-  private val trainSection1 = TrainSection(4, section1.id, sectionActor.ref)
-  private val trainSection2 = TrainSection(3, section2.id, sectionActor.ref)
+  val sectionActor1 = TestProbe()
+  val sectionActor2 = TestProbe()
+  val section1 = Section("A-B",1)
+  val section2 = Section("B-C",1)
+  private val trainSection1 = TrainSection(4, section1.id, sectionActor1.ref)
+  private val trainSection2 = TrainSection(3, section2.id, sectionActor2.ref)
   val trainSections = List(trainSection1, trainSection2)
 
 
@@ -64,7 +65,8 @@ class TrainActorSpec(_system: ActorSystem) extends TestKit(_system) with Implici
         val trainId = 3
         val dut = system.actorOf(TrainActor.props(trainId, startTime, trainSections))
         advanceClockTo(7,dut)
-        sectionActor.expectMsg(EnterSection(trainId))
+        sectionActor2.expectMsg(EnterSection(trainId))
+        dut ! SectionEntered(section2)
       }
     }
 
